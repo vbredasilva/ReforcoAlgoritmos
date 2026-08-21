3056b26ca979a171178b42cc70bec31a7f672909
Vinicius Breda <vinicius_breda_silva@hotmail.com>
2026-08-21 20:42:31 -0300

    Exercicio 1 lista de reforço

---
+++ Exercicios/Ex1Reforco.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	int num1, num2, soma;
+	
+	printf("Informe o numero 1: ");
+	scanf("%d", &num1);
+	
+	printf("\nInforme o numero 2: ");
+	scanf("%d", &num2);
+	
+	soma = num1 + num2;
+	
+	printf("\nO valor da soma eh: %d", soma);
+	
+}
